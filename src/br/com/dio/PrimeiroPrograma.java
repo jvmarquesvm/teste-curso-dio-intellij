ad8062f43facca38a26a1d61200c0dbad187ec78
Joao <joaovictor2@hotmail.com>
2022-01-13 22:20:49 -0300

    Criando classe livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,6 +2,8 @@ package br.com.dio;
 
 import model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         /*
@@ -11,5 +13,45 @@ public class PrimeiroPrograma {
 
         Gato gato = new Gato();
         System.out.println("Gato: " + gato);
+    }
+}
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro() {
+    }
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(nome, livro.nome) && Objects.equals(numPaginas, livro.numPaginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numPaginas);
     }
 }
