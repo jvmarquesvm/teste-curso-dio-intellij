287857df7a4a58f5a123a9863109ff35c016a1f2
Joao <joaovictor2@hotmail.com>
2022-01-20 00:37:14 -0300

    Trabalhando com os Testes DIO

---
+++ src/br/com/dio/testes/MudancaContinua2.java
@@ -0,0 +1,8 @@
+package br.com.dio.testes;
+
+public class MudancaContinua2 {
+    public static void main(String[] args) {
+        Double horas = 95.5 / 24;
+        System.out.println( horas);
+    }
+}
