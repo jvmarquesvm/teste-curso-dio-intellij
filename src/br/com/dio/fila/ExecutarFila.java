03dcdec549830d5989226bf2a83a267bc350db5d
Joao <joaovictor2@hotmail.com>
2022-03-01 05:59:33 -0300

    Implementando a classe de teste da fila.

---
+++ src/br/com/dio/fila/ExecutarFila.java
@@ -0,0 +1,23 @@
+package br.com.dio.fila;
+
+public class ExecutarFila {
+    public static void main(String[] args) {
+        Fila fila = new Fila();
+        fila.enqueue(new No("primeiro"));
+        fila.enqueue(new No("segundo"));
+        fila.enqueue(new No("terceiro"));
+        fila.enqueue(new No("quarto"));
+
+        System.out.println(fila);
+        System.out.println(fila.dequeue());
+        System.out.println(fila);
+
+        fila.enqueue(new No("ultimo"));
+        System.out.println(fila);
+        System.out.println(fila.first());
+        System.out.println(fila);
+        System.out.println(fila.dequeue());
+        System.out.println(fila);
+
+    }
+}
